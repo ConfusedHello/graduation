30ed3a53acfec1d1efae2459c81c6076df612ed7
Brandon Zhou <s1072489@haileybury.com.au>
2025-12-03 10:14:32 +1100

    Fix property errors

--- components/InfiniteGallery.tsx
+++ components/InfiniteGallery.tsx
@@ -460,8 +460,9 @@ function GalleryScene({
 				const worldZ = plane.z - depthRange / 2;
 
 				// Calculate scale to maintain aspect ratio
-				const aspect = texture.image
-					? texture.image.width / texture.image.height
+				const image = texture.image as HTMLImageElement | undefined;
+				const aspect = image?.width && image?.height
+					? image.width / image.height
 					: 1;
 				const scale: [number, number, number] =
 					aspect > 1 ? [2 * aspect, 2, 1] : [2, 2 / aspect, 1];
